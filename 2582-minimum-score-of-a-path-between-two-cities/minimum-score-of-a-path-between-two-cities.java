a574869c47e665e238cf4dedd15c510ffdf705bb
sujal-17-nagpal <sujaln0609y@gmail.com>
2026-07-04 14:23:10 +0530

    Time: 77 ms (12.94%) | Memory: 187.3 MB (7.46%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.java
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.java
@@ -1,42 +1,55 @@
 class Solution {
+
+    int ans;
+    ArrayList<ArrayList<int[]>> adj;
+    boolean[] vis;
+
+    void dfs(int node, int id, int mn) {
+        ans = Math.min(ans, mn);
+
+        if (id != -1) {
+            vis[id] = true;
+        }
+
+        for (int[] e : adj.get(node)) {
+            int nei = e[0];
+            int wt = e[1];
+            int edgeId = e[2];
+
+            if (vis[edgeId]) {
+                continue;
+            }
+
+            dfs(nei, edgeId, Math.min(mn, wt));
+        }
+    }
+
     public int minScore(int n, int[][] roads) {
-        ArrayList<ArrayList<int []>> adj = new ArrayList<>();
-        for(int i = 0;i<n;i++){
+
+        adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int [] a : roads){
-            int u = a[0];
-            int v = a[1];
+
+        int id = 0;
+
+        for (int[] a : roads) {
+            int u = a[0] - 1;
+            int v = a[1] - 1;
             int d = a[2];
-            adj.get(u-1).add(new int[]{v-1,d});
-            adj.get(v-1).add(new int[]{u-1,d});
+
+            adj.get(u).add(new int[]{v, d, id});
+            adj.get(v).add(new int[]{u, d, id});
+
+            id++;
         }
-        PriorityQueue<int []> q = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        int [] dis = new int[n];
-        Arrays.fill(dis,Integer.MAX_VALUE);
-        // dis[0] = 0;
-        q.add(new int[]{0,Integer.MAX_VALUE});
-        int ans = Integer.MAX_VALUE;
-        while(!q.isEmpty()){
-            int [] a = q.poll();
-            // System.out.println(a[0]+" "+a[1]);
-            int node = a[0];
-            int mn = a[1];
-            
-            // if(node == n-1) {
-            //     ans = Math.min(ans,mn);
-            //     continue;
-            // }
-            for(int [] e : adj.get(node)){
-                int nei = e[0];
-                int cost = e[1];
-                int newMn = Math.min(mn,cost);
-                if(newMn < dis[nei]){
-                    dis[nei] = newMn;
-                    q.add(new int[]{nei,newMn});
-                }
-            }
-        }
-        return dis[n-1];
+
+        vis = new boolean[id];
+        ans = Integer.MAX_VALUE;
+
+        dfs(0, -1, Integer.MAX_VALUE);
+
+        return ans;
     }
 }
