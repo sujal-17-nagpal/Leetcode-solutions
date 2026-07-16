712f18c86de5fe9073f67ceb8ffefe92b27acc49
sujal-17-nagpal <sujaln0609y@gmail.com>
2026-07-16 17:25:41 +0530

    Time: 355 ms (8.65%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,3 +1,3 @@
-select id,movie,description,rating from Cinema
-where description != "boring" and id%2 = 1
+select * from Cinema
+where description not like "boring" and id%2 = 1
 order by rating desc  
