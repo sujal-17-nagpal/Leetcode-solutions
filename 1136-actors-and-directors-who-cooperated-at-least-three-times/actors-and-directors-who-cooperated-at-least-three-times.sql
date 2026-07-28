9f212f4051b76a64b4b544480ec7bc6a46cebf58
sujal-17-nagpal <sujaln0609y@gmail.com>
2026-07-28 16:24:54 +0530

    Time: 385 ms (49.13%) | Memory: 0B (100.00%) - LeetSync

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,2 +1,3 @@
-select distinct t1.actor_id,t1.director_id from ActorDirector as t1
-where 3 <= (select count(*) from ActorDirector as t2 where t1.actor_id = t2.actor_id and t1.director_id = t2.director_id)
+select actor_id,director_id from ActorDirector
+group by actor_id,director_id
+having count(*)>=3
